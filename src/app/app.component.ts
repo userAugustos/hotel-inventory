a4f26dd0af3e967e7c2573ff30c14c361baa7b7a
userAugustos <felipe_augustos84@outlook.com.br>
2023-01-25 16:24:17 -0300

    chore: more debuggin and notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+// components são seus views (MVC)
+
+/*
+* Vamos falar desse decorator @Component
+* Quando cria um component, o que você realmente cria é uma view, o que vai acontecer na tela
+* *selector*: então, se você for ao nosso index.html, vai ver que troquei a tag html, para app-start (assim como aqui a baixo)
+* assim, nosso selector, sabe onde jogar nosso component. (lembrando que se mudar o prefixo, vai precisar mudar no angular.json também)
+* *templateUrl*: É o html como qual nosso component vai conversar, passar o css e o js, e o que ele vai mandar pro nosso selector
+* styleUrls: É o css que passamos ao nosso html
+*
+* */
+
 @Component({
-  selector: 'app-root',
+  selector: 'hotel-start',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hotelinventory';
+  title = 'Hotel Inventory';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // qualquer component, directive ou pipes, precisam estar declarados aqui
+    AppComponent,
+    RoomsComponent
   ],
-  imports: [
+  imports: [ // qualquer modulo que precisar ser iniciado
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // qual o primeiro component a ser carregado?
 })
-export class AppModule { }
+export class AppModule { } // root module
